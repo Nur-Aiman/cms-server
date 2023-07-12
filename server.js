f1c6f182afb35358f7c38fa22c3ef92aa492be0e
Nur-Aimanecho <nur_aiman71099@yahoo.com>
2023-07-12 12:18:10 +0800

    test

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
-import app from './app.js'
+const app = require('./app.js')
 
-
-const PORT = 8080;
-
+const PORT = 8080
 
 app.listen(PORT, () => {
-    console.log("Server run on port 8080");
-});
+  console.log('Server run on port 8080')
+})
