a451622b904464702921d04506a99f32bee85ec0
Nur-Aiman <64392281+Nur-Aiman@users.noreply.github.com>
2024-01-07 16:29:09 +0800

    modify counsellor login cookie

--- controllers/counsellorController.js
+++ controllers/counsellorController.js
@@ -55,7 +55,7 @@ module.exports = {
                             `Counsellor login successful. Current user: Email: ${email} Password: ${password} Token: ${accessToken} Role: ${userRole}`
                         )
 
-                        res.cookie('access_token', accessToken)
+                        // res.cookie('access_token', accessToken)
                             // {
                             //   sameSite: false,
                             //   secure: true,
